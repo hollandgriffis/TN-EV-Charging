214298b846dba16b1e9c30dc53f9737d317254fb
Holland Griffis <hollandgriffis@gmail.com>
2021-12-14 15:30:44 -0600

    12_14 upload 1

--- scripts/csv_to_pg.py
+++ scripts/csv_to_pg.py
@@ -3,5 +3,6 @@ from sqlalchemy import create_engine
 df = pd.read_csv('../data/tn_gas_stations.csv')
 df.columns = [c.lower() for c in df.columns] #postgres doesn't like capitals or spaces
 df.columns = df.columns.str.replace(' ', '_')
+df.columns = df.columns.str.replace(':', '_')
 engine = create_engine('postgresql://postgres:password@localhost:5432/postgres')
 df.to_sql("tn_gas_stations", engine)
